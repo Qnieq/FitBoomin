0282005f44a8fe7ee02ba0f2c590e72629a59065
Mike <106514673+Qnieq@users.noreply.github.com>
2023-10-14 15:15:49 +0300

    Update LogoWhite.jsx

--- src/components/ui/Logo/LogoWhite.jsx
+++ src/components/ui/Logo/LogoWhite.jsx
@@ -3,7 +3,7 @@ import styles from "./Logo.module.css"
 
 const LogoWhite = () => {
     return (
-        <Link to={'/home'}>
+        <Link to={'/FitBoomin/'}>
             <div className={styles.title}>
                 <img src="images/logo-white.png" alt="fitboomin" className={styles.logo}/>
                 <h2 className={styles.nickname}>FitBoo<span className={styles.nickname_min}>min</span></h2>
