51e959df465c313f8529f4284b30ec75827b176b
Mike <misha.o.son@gmail.com>
2023-11-06 22:06:27 +0300

    adapt

--- src/components/ui/ProgItems/ProgItems.jsx
+++ src/components/ui/ProgItems/ProgItems.jsx
@@ -3,40 +3,40 @@ import styles from "./ProgItems.module.css"
 const ProgItems = () => {
     return (
         <div className={styles.progList}>
-                    <div className={styles.progItem}>
-                        <div className={styles.progItem_icon}>
-                            <img src="images/Black_biceps.png" alt="" />
-                        </div>
-                        <div className={styles.progItem_text}>
-                            <h5>Personal Training</h5>
-                            <p>
-                                It’s a long estabilished fact that a <br />reader will be distracted by the readable content
-                            </p>
-                        </div>
-                    </div>
-                    <div className={styles.progItem}>
-                        <div className={styles.progItem_icon}>
-                            <img src="images/sport-dumbbell.png" alt="" />
-                        </div>
-                        <div className={styles.progItem_text}>
-                            <h5>Workout Programs</h5>
-                            <p>
-                                It’s a long estabilished fact that a <br />reader will be distracted by the readable content
-                            </p>
-                        </div>
-                    </div>
-                    <div className={styles.progItem}>
-                        <div className={styles.progItem_icon}>
-                            <img src="images/sport-slim-waist.png" alt="" />
-                        </div>
-                        <div className={styles.progItem_text}>
-                            <h5>Program & Diet</h5>
-                            <p>
-                                It’s a long estabilished fact that a <br />reader will be distracted by the readable content
-                            </p>
-                        </div>
-                    </div>
+            <div className={styles.progItem}>
+                <div className={styles.progItem_icon}>
+                    <img src="images/Black_biceps.png" alt="" />
                 </div>
+                <div className={styles.progItem_text}>
+                    <h5>Personal Training</h5>
+                    <p>
+                        It’s a long estabilished fact that a <br />reader will be distracted by the readable content
+                    </p>
+                </div>
+            </div>
+            <div className={styles.progItem}>
+                <div className={styles.progItem_icon}>
+                    <img src="images/sport-dumbbell.png" alt="" />
+                </div>
+                <div className={styles.progItem_text}>
+                    <h5>Workout Programs</h5>
+                    <p>
+                        It’s a long estabilished fact that a <br />reader will be distracted by the readable content
+                    </p>
+                </div>
+            </div>
+            <div className={styles.progItem}>
+                <div className={styles.progItem_icon}>
+                    <img src="images/sport-slim-waist.png" alt="" />
+                </div>
+                <div className={styles.progItem_text}>
+                    <h5>Program & Diet</h5>
+                    <p>
+                        It’s a long estabilished fact that a <br />reader will be distracted by the readable content
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/components/ui/TopBar/TopBarBlue.jsx
+++ src/components/ui/TopBar/TopBarBlue.jsx
@@ -1,8 +1,26 @@
 import { NavLink, Link } from "react-router-dom";
 import LogoWhite from "../Logo/LogoWhite";
 import styles from "./TopBarBlue.module.css"
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import MenuOpenRoundedIcon from '@mui/icons-material/MenuOpenRounded';
+import IconButton from '@mui/material/IconButton';
+import { useRef, useState } from "react";
+import { CSSTransition } from 'react-transition-group';
 
 const TopBarBlue = () => {
+    const [actions, setActions] = useState({
+        openMenu: false
+    });
+    const [inProp, setInProp] = useState(false);
+
+    const handleClickOpenMenu = () => {
+        setActions({ ...actions, openMenu: !actions.openMenu });
+        setInProp(!inProp)
+    };
+
+    // const handleMouseOpenMenu = (event) => {
+    //     event.preventDefault();
+    // };
 
     return (
         <div className={styles.cssContainer}>
@@ -42,6 +60,59 @@ const TopBarBlue = () => {
                                 })}>Contact</NavLink>
                             </li>
                         </ul>
+                    </div>
+                    <div className={styles.navBarMob}>
+                        <IconButton 
+                        color=""
+                        onClick={handleClickOpenMenu} 
+                        className={styles.menuIconButton}>
+                                {actions.openMenu ? <MenuOpenRoundedIcon /> : <MenuRoundedIcon />}
+                        </IconButton>
+                        {actions.openMenu ?
+                            <div className={styles.menu_mob}>
+                                <ul className={styles.menu_list_mob}>
+                                    <li className={styles.menu_item_mob}>
+                                        <NavLink  to="/FitBoomin/" style={({ isActive }) => ({
+                                            color: isActive ? '#fff' : '#fff',
+                                            opacity: isActive ? 1 : 0.56,
+                                        })}>Home</NavLink>
+                                    </li>
+                                    <li className={styles.menu_item}>
+                                        <NavLink to="/programs" style={({ isActive }) => ({
+                                            color: isActive ? '#fff' : '#fff',
+                                            opacity: isActive ? 1 : 0.56,
+                                        })}>Programs</NavLink>
+                                    </li>
+                                    <li className={styles.menu_item}>
+                                        <NavLink to="/trainers" style={({ isActive }) => ({
+                                            color: isActive ? '#fff' : '#fff',
+                                            opacity: isActive ? 1 : 0.56,
+                                        })}>Trainers</NavLink>
+                                    </li>
+                                    <li className={styles.menu_item}>
+                                        <NavLink to="/membership" style={({ isActive }) => ({
+                                            color: isActive ? '#fff' : '#fff',
+                                            opacity: isActive ? 1 : 0.56,
+                                        })}>Membership</NavLink>
+                                    </li>
+                                    <li className={styles.menu_item}>
+                                        <NavLink to="/contact" style={({ isActive }) => ({
+                                            color: isActive ? '#fff' : '#fff',
+                                            opacity: isActive ? 1 : 0.56,
+                                        })}>Contact</NavLink>
+                                    </li>
+                                    <li className={styles.menu_item}>
+                                        <NavLink to="/sign-in" style={() => ({
+                                            color: '#fff'
+                                        })}>Sign In</NavLink>
+                                    </li>
+                                    <li className={styles.menu_item}>
+                                        <NavLink to="/sign-up" style={() => ({
+                                            color: '#bcff5e'
+                                        })}>Sign Up</NavLink>
+                                    </li>
+                                </ul>
+                            </div> : <></> }
                     </div>
                     <div className={styles.buy_menu}>
                         {/* <Link to="/membership/cart"><img src="/public/images/buy.png" alt="buycart" className={styles.buy}/><span className={styles.count}>{cart.length}</span></Link> */}
