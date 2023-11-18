10a2cf28094e61c2257709e1ee673c503a6653d6
Mike <misha.o.son@gmail.com>
2023-11-18 22:55:12 +0300

    site

--- src/store/forgotPassword/forgotPassword.actions.js
+++ src/store/forgotPassword/forgotPassword.actions.js
@@ -4,7 +4,6 @@ import axios from "axios";
 export const updatePassword = createAsyncThunk(
   "password/update",
   async (id) => {
-    console.log(id);
     const resp = await axios.put(`https://fitboomin--mishaoson.repl.co/users/${id[0]}`, {
       id: id[0],
       email: id[0],
