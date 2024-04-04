e8e859eec943970d8d308ff5b240cf2ea3163184
Mike <misha.o.son@gmail.com>
2024-04-04 11:03:27 +0300

    site

--- src/components/screens/AuthorizationPg/SignUp/SignUpContainer.jsx
+++ src/components/screens/AuthorizationPg/SignUp/SignUpContainer.jsx
@@ -73,10 +73,6 @@ const SignUpContainer = () => {
                 <div className={styles.content}>
                     <h3>Welcome to Work<span>out</span></h3>
                     <div className={styles.variants}>
-                        <div className={styles.cta}>
-                            <button><img src="images/google.png" alt="" />Continue with Google</button>
-                            <button><img src="images/icon-apple.png" alt="" />Continue with Apple</button>
-                        </div>
                         <div className={styles.login}>
                             <span></span>
                             <h6>Or Sign Up with</h6>
