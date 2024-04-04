8d60fcbb20353b423ff827e7a895ae946f49cfe6
Mike <106514673+Qnieq@users.noreply.github.com>
2024-04-04 14:46:24 +0300

    Update register.actions.js

--- src/store/registration/register.actions.js
+++ src/store/registration/register.actions.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const sendUsers = createAsyncThunk(
   "users/send",
   async (user) => {
-    const resp = await axios.post('https://fitboomin--mishaoson.repl.co/users', user);
+    const resp = await axios.post('https://e950bb45-80e2-4f5f-94c9-a698ea4952bf-00-2t6ee8m0q1zkz.riker.replit.dev/users', user);
     console.log(resp.data);
     return resp.data
   }
