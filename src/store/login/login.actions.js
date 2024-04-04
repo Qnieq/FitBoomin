a8abd21e7cee5cbbe17dd26be8ebc31710b06b49
Mike <106514673+Qnieq@users.noreply.github.com>
2024-04-04 14:48:52 +0300

    Update login.actions.js

--- src/store/login/login.actions.js
+++ src/store/login/login.actions.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const getAuthUsers = createAsyncThunk(
   "users/get",
   async (id) => {
-    const resp = await axios.get(`https://fitboomin--mishaoson.repl.co/users/${id}`);
+    const resp = await axios.get(`https://e950bb45-80e2-4f5f-94c9-a698ea4952bf-00-2t6ee8m0q1zkz.riker.replit.dev/users/${id}`);
 
     return resp.data
   }
