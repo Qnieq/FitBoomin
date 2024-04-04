ab13ff5f889d52326fadeaae37a360de1515bc25
Mike <106514673+Qnieq@users.noreply.github.com>
2024-04-04 14:49:55 +0300

    Update forgotPassword.actions.js

--- src/store/forgotPassword/forgotPassword.actions.js
+++ src/store/forgotPassword/forgotPassword.actions.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const updatePassword = createAsyncThunk(
   "password/update",
   async (id) => {
-    const resp = await axios.put(`https://fitboomin--mishaoson.repl.co/users/${id[0]}`, {
+    const resp = await axios.put(`https://e950bb45-80e2-4f5f-94c9-a698ea4952bf-00-2t6ee8m0q1zkz.riker.replit.dev/users/${id[0]}`, {
       id: id[0],
       email: id[0],
       password: id[1]
@@ -17,7 +17,7 @@ export const updatePassword = createAsyncThunk(
 export const getUsersForNewPass = createAsyncThunk(
   "users/getForget",
   async (id) => {
-    const resp = await axios.get(`https://fitboomin--mishaoson.repl.co/users/${id}`);
+    const resp = await axios.get(`https://e950bb45-80e2-4f5f-94c9-a698ea4952bf-00-2t6ee8m0q1zkz.riker.replit.dev/users/${id}`);
 
     return resp.data
   }
